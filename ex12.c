87a0d2e1cdd9c17b821c784486ec32437761e01f
laurakrs <82679218+laurakrs@users.noreply.github.com>
2021-08-09 18:35:20 -0300

    arrumei 12

--- ex12.c
+++ ex12.c
@@ -14,13 +14,19 @@ int main(void){
     printf("\nEntre com um numero binario de 5 digitos: ");
     scanf("%d", &numeroBinario);
 
-    n1 = numeroBinario % 10;
-    n2 = (numeroBinario - n1) % 100; 
-    n3 = (numeroBinario-n2-n1)%1000;
-    n4 = (numeroBinario-n3-n2-n1)%10000;
-    n5 = (numeroBinario-n4-n3-n2-n1)/10000;
+    // 11111
+    n1 = numeroBinario % 10; // 1
+    printf("%d", n1);
+    n2 = (numeroBinario - n1) % 100; //11110 %100 = 10 
+    printf("%d", n2);
+    n3 = (numeroBinario-n2-n1)%1000; //11100 % 1000 = 100
+    printf("%d", n3);
+    n4 = (numeroBinario-n3-n2-n1)%10000; //11000 % 10000 = 1000
+    printf("%d", n4);
+    n5 = (numeroBinario-n4-n3-n2-n1)/10000; //10000 / 10000 = 1 
+    printf("%d", n5);
 
-    numDecimal = n1*2^0 + n2*2 + n3*2^2 + n4*2^3 + n5*2^4;
+    numDecimal = n1*1 + (n2/10)*2 + (n3/100)*4 + (n4/1000)*8 + n5*16;
 
     printf("Numero decimal: %d", numDecimal);
     
